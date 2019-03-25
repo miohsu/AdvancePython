244878d9dccc6c5d770956b39c57458f40fd84bd
miohsu <kunieshow@163.com>
2019-03-25 10:36:10 +0800

    325

--- Chapter_04/class_var.py
+++ Chapter_04/class_var.py
@@ -1,15 +1,15 @@
-
 class A(object):
     a = 1000
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 
-a = A(1,2)
-b = A(3,5)
-print(A.a,a.a,a.x,a.y)
+a = A(1, 2)
+b = A(3, 5)
+print(A.a, a.a, a.x, a.y)
 a.a = 2000
-print(A.a,a.a,a.x,a.y)
+print(A.a, a.a, a.x, a.y)
 A.a = 3000
-print(A.a,a.a,a.x,a.y)
+print(A.a, a.a, a.x, a.y)
