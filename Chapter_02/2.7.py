63837753c0c5ef5d2b03ba1f7b2f60430167586a
miohsu <kunieshow@163.com>
2019-03-26 12:28:51 +0800

    2.7

---
+++ Chapter_02/2.7.py
@@ -0,0 +1,11 @@
+"""
+    list.sort 方法和内置函数 sorted
+    1.
+    list.sort: 方法是就地排序
+    sorted: 返回一个新序列
+
+    2.
+    sort 和 sorted 都有两个关键字参数 key 和 reverse
+    reverse: 默认为 False，升序排序
+    key: 一个只有一个参数的函数，这个函数会被用在序列里的每一个元素上，所产生的结果将是排序算法所依赖的对比关键字
+"""
