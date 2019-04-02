7c59e4c08ed596655c81acb8ae5a35f4a322bff8
miohsu <kunieshow@163.com>
2019-04-02 17:37:59 +0800

    3.1

--- Chapter_03/3.1.py
+++ Chapter_03/3.1.py
@@ -20,6 +20,13 @@
 
     5.
     字典的构造方法
+    a = dict(one=1, two=2, three=3)
+    b = {'one':1, 'two':2, 'three':3}
+    c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+    d = dict([('one', 1), ('two', 2), ('three', 3)])
+    e = dict({'one':1, 'two':2, 'three':3})
+    a == b == c == d == e
+    True
 """
 
 from collections import abc
