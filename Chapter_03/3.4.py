e69f6f470c87f512fea0fbdcf9ef211209cecc97
miohsu <kunieshow@163.com>
2019-04-06 11:54:28 +0800

    3.5

--- Chapter_03/3.4.py
+++ Chapter_03/3.4.py
@@ -4,8 +4,17 @@
     - 通过 defaultdict 对象
     - 在 dict 类中实现 __missing__ 方法
 
+    1.
     __missing__ 方法
     该方法只会被 __getitem__ 调用，对 get 方法和 __contains__ 方法不起作用
+
+    2.
+    当有非字符串的键被查找时，StrKeyDict0 可以把在查询时把非字符串的键转换成字符串
+    在 __contains__ 方法中不能使用 key in dict 否则会出现递归调用，因此需要使用 key in dict.keys()
+
+    3.
+    Python3 中 dict.keys() 返回格式为 dict_keys 视图
+    Python2 中返回格式为 list
 """
 
 from collections import defaultdict
@@ -13,3 +22,21 @@ from collections import abc
 
 d = {1: 1, 2: 2}
 d.get(1)
+res = d.keys()
+pass
+
+
+class StrKeyDict0(dict):
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError(key)
+        return self[str(key)]
+
+    def get(self, k, default=None):
+        try:
+            return self[k]
+        except KeyError:
+            return default
+
+    def __contains__(self, key):
+        return key in self.keys() or str(key) in self.keys()
---
+++ Chapter_03/3.5.py
@@ -0,0 +1,30 @@
+"""
+    字典的变种
+    1.
+    collections.OrderedDict
+    这个类型在添加键的时候会保持顺序，因此键的迭代次序总是一致的。
+    popitem 实现的是一个栈，可以接收一个参数 last=False 变为队列
+
+    2.
+    collections.ChainMap
+    该类型可以容纳数个不同的映射对象，然后在进行键查找操作时，这些对象会被当作一个整体被逐个查找
+
+    3.
+    collections.Counter
+
+
+    4.
+    collections.UserDict
+"""
+
+from collections import OrderedDict
+
+init_data = {1:'a',2:'b'}
+
+od = OrderedDict()
+od.update(init_data)
+od[3] = 'c'
+od[4] = 'd'
+print(od)
+print(od.popitem())
+print(od.popitem())
